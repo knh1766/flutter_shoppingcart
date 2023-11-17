1ea7b192381729c9ea2627c0151e077d7b7aec68
knh1766 <knh1766@naver.com>
2023-11-17 11:58:46 +0900

    shoppingcart

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primaryColor: kPrimaryColor,
+    scaffoldBackgroundColor: kPrimaryColor,
+  );
+}
